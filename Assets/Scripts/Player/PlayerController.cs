618c21c6625498a3b8e13c44cab6499dc146d383
Felix Petiz Bonilho <62768869+Felix-xilef@users.noreply.github.com>
2024-07-18 09:22:47 -0300

    finished player movement and animation

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -9,29 +9,118 @@ public class PlayerController : MonoBehaviour {
     [SerializeField]
     private float speed;
 
+    [SerializeField]
+    private float turnSpeed;
+
+    [SerializeField]
+    private float smoothTime;
+
+
+    private Rigidbody playerRigidbody;
+
+    private Vector2 currentSmoothDampVelocity;
+
+
+    private Vector3 initialPosition;
+    private Quaternion initialRotation;
+
+
+    private void Start() {
+        playerRigidbody = GetComponent<Rigidbody>();
+
+        initialPosition = transform.position;
+        initialRotation = transform.rotation;
+
+        GameManager.Instance.onGameStateChanged += GameStateChanged;
+    }
+
+    private void OnDestroy() {
+        if (GameManager.Instance != null) {
+            GameManager.Instance.onGameStateChanged -= GameStateChanged;
+        }
+    }
 
     private void Update() {
-        var newPosition = transform.position + (Vector3) (speed * Time.deltaTime * movementAction.action.ReadValue<Vector2>());
+        if (GameManager.Instance.GameState == GameState.Running) {
+            CalculateNewVelocity();
 
-        var lowerBoundary = GameManager.Instance.GameLowerBoundaries;
-        var upperBoundary = GameManager.Instance.GameUpperBoundaries;
+            CheckBoundaries();
 
-        if (newPosition.x < lowerBoundary.x) {
-            newPosition.x = lowerBoundary.x;
-
-        } else if (newPosition.x > upperBoundary.x) {
-            newPosition.x = upperBoundary.x;
+            RotateByVelocity();
         }
-
-        if (newPosition.y < lowerBoundary.y) {
-            newPosition.y = lowerBoundary.y;
-
-        } else if (newPosition.y > upperBoundary.y) {
-            newPosition.y = upperBoundary.y;
-        }
-
-        transform.position = newPosition;
     }
+
+
+    private void CalculateNewVelocity() {
+        var newVelocity = speed * movementAction.action.ReadValue<Vector2>();
+
+        newVelocity = Vector2.SmoothDamp(
+            playerRigidbody.velocity,
+            newVelocity,
+            ref currentSmoothDampVelocity,
+            smoothTime
+        );
+
+        playerRigidbody.velocity = newVelocity;
+    }
+
+    private void CheckBoundaries() {
+        var lowerBoundaries = GameManager.Instance.GameLowerBoundaries;
+        var upperBoundaries = GameManager.Instance.GameUpperBoundaries;
+
+        var position = transform.position;
+        var velocity = playerRigidbody.velocity;
+
+        if (position.x < lowerBoundaries.x) {
+            position.x = lowerBoundaries.x;
+            velocity.x = 0;
+
+        } else if (position.x > upperBoundaries.x) {
+            position.x = upperBoundaries.x;
+            velocity.x = 0;
+        }
+
+        if (position.y < lowerBoundaries.y) {
+            position.y = lowerBoundaries.y;
+            velocity.y = 0;
+
+        } else if (position.y > upperBoundaries.y) {
+            position.y = upperBoundaries.y;
+            velocity.y = 0;
+        }
+
+        transform.position = position;
+        playerRigidbody.velocity = velocity;
+    }
+
+    private void RotateByVelocity() {
+        var direction = Quaternion.LookRotation(
+            playerRigidbody.velocity + (Vector3.forward * speed)
+        );
+
+        var rotation = Quaternion.Slerp(
+            transform.rotation,
+            direction,
+            turnSpeed * Time.deltaTime
+        );
+
+        playerRigidbody.MoveRotation(
+            rotation
+        );
+    }
+
+
+    private void GameStateChanged(GameState newState) {
+        if (newState == GameState.Stopped) {
+            playerRigidbody.velocity = Vector3.zero;
+
+            transform.SetPositionAndRotation(
+                initialPosition,
+                initialRotation
+            );
+        }
+    }
+
 
     private void OnTriggerEnter(Collider other) {
         if (other.gameObject.TryGetComponent<PointController>(out var pointController)) {
